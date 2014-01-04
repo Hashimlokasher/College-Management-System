c251d3ec53935879fb67407de732e6fd1bdb6cb2
Hashim Lokasher <Hashimlokasher@gmail.com>
2014-01-04 20:28:30 +0500

    initial version
    Everything is working fine

---
+++ frmLogin.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace College_Management_System
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (cmbUsertype.Text == "")
+            {
+                MessageBox.Show("Please select user type", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                cmbUsertype.Focus();
+                return;
+            }
+            if (txtUserName.Text == "")
+            {
+                MessageBox.Show("Please enter user name", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtUserName.Focus();
+                return;
+            }
+            if (txtPassword.Text == "")
+            {
+                MessageBox.Show("Please enter password", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtPassword.Focus();
+                return;
+            }
+            try
+            {
+                SqlConnection myConnection = default(SqlConnection);
+                myConnection = new SqlConnection("Data Source=.\\SqlExpress; Integrated Security=True; AttachDbFilename=|DataDirectory|\\CMS_DB.mdf; User Instance=true;");
+
+                SqlCommand myCommand = default(SqlCommand);
+
+                myCommand = new SqlCommand("SELECT UserType,Username,password FROM Users WHERE UserType = @usertype AND Username = @username AND password = @UserPassword", myConnection);
+                SqlParameter uType = new SqlParameter("@usertype", SqlDbType.NChar);
+
+                SqlParameter uName = new SqlParameter("@username", SqlDbType.NChar);
+
+                SqlParameter uPassword = new SqlParameter("@UserPassword", SqlDbType.NChar);
+
+                uType.Value = cmbUsertype.Text;
+                uName.Value = txtUserName.Text;
+                uPassword.Value = txtPassword.Text;
+
+                myCommand.Parameters.Add(uType);
+                myCommand.Parameters.Add(uName);
+                myCommand.Parameters.Add(uPassword);
+
+                myCommand.Connection.Open();
+
+                SqlDataReader myReader = myCommand.ExecuteReader(CommandBehavior.CloseConnection);
+
+                if (myReader.Read() == true)
+                {
+
+                   
+                        int i;
+                        ProgressBar1.Visible = true;
+                        ProgressBar1.Maximum = 5000;
+                        ProgressBar1.Minimum = 0;
+                        ProgressBar1.Value = 4;
+                        ProgressBar1.Step = 1;
+
+                        for (i = 0; i <= 5000; i++)
+                        {
+                            ProgressBar1.PerformStep();
+                        }
+
+
+
+
+                        this.Hide();
+                        frmMainMenu frm = new frmMainMenu();
+                        frm.User.Text = txtUserName.Text;
+                        frm.UserType.Text = cmbUsertype.Text;
+                        frm.Show();
+
+                    }
+                
+
+                else
+                {
+                    MessageBox.Show("Login is Failed...Try again !", "Login Denied", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    txtUserName.Clear();
+                    txtPassword.Clear();
+                    txtUserName.Focus();
+
+                }
+                if (myConnection.State == ConnectionState.Open)
+                {
+                    myConnection.Dispose();
+                }
+
+              
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+      
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ProgressBar1.Visible = false;
+            cmbUsertype.Focus();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            return;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Hide();
+            frmChangePassword frm = new frmChangePassword();
+            frm.Show();
+            frm.txtUserName.Text = "";
+            frm.txtNewPassword.Text = "";
+            frm.txtOldPassword.Text = "";
+            frm.txtConfirmPassword.Text = "";
+        }
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Hide();
+            frmRecoveryPassword frm = new frmRecoveryPassword();
+            frm.txtEmail.Focus();
+            frm.Show();
+        }
+    }
+}
